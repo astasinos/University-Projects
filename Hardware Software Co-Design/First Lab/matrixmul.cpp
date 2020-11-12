3394e20218ae145794df80fd09afaeb0ae78641d
astasinos <56511877+astasinos@users.noreply.github.com>
2020-11-12 21:59:38 +0200

    Update matrixmul.cpp

--- Hardware Software Co-Design/First Lab/matrixmul.cpp
+++ Hardware Software Co-Design/First Lab/matrixmul.cpp
@@ -1,11 +1,9 @@
 #include "matrixmul.h"
 
 // for tripcount pragmas
-	const int n_iter = n;
-	const int p_iter = p;
-	const int m_iter = m;
-
-
+const int n_iter = n;
+const int p_iter = p;
+const int m_iter = m;
 
 void ARRAY_COPY_ALL(ap_uint<8> inArray1[n][m],ap_uint<8> outArray1[n][m],ap_uint<8> inArray2[m][p],ap_uint<8> outArray2[m][p]){
 #pragma HLS DATAFLOW
@@ -35,18 +33,11 @@ void ARRAY_COPY_ALL(ap_uint<8> inArray1[n][m],ap_uint<8> outArray1[n][m],ap_uint
 void matrix_mul_hw(ap_uint<8> inArray1[n][m], ap_uint<8> inArray2[m][p],
 		unsigned int outArray[n][p]){
 
-
-
 	ap_uint<8> BRAM_in1[n][m];
 	ap_uint<8> BRAM_in2[m][p];
 
 	#pragma HLS ARRAY_PARTITION variable=BRAM_in1 cyclic factor=m_iter dim=2
 	#pragma HLS ARRAY_PARTITION variable=BRAM_in2 cyclic factor=m_iter dim=1
-
-
-
-
-
 
 	ARRAY_COPY_ALL(inArray1,BRAM_in1,inArray2,BRAM_in2);
 
@@ -54,7 +45,6 @@ void matrix_mul_hw(ap_uint<8> inArray1[n][m], ap_uint<8> inArray2[m][p],
 
 	for(int i = 0; i < n; i++){
 	#pragma HLS loop_tripcount min=n_iter max=n_iter
-
 
 		for(int j = 0; j < p; j++) {
 		#pragma HLS loop_tripcount min=p_iter max=p_iter
@@ -66,15 +56,10 @@ void matrix_mul_hw(ap_uint<8> inArray1[n][m], ap_uint<8> inArray2[m][p],
 				#pragma HLS loop_tripcount min=m_iter max=m_iter
 
 				result += BRAM_in1[i][k] * BRAM_in2[k][j];
-
-			}
+}
 
 			outArray[i][j] = result;
-
 		}
 
 	}
-
-
-
 }
