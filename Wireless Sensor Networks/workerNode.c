fe30f97b8ddf2d356afea815c21fd01833f5497b
astasinos <56511877+astasinos@users.noreply.github.com>
2021-05-07 17:28:42 +0300

    Update workerNode.c

--- Wireless Sensor Networks/workerNode.c
+++ Wireless Sensor Networks/workerNode.c
@@ -2,7 +2,21 @@
 
 Code that runs on the sensor nodes located on the helmets of the workers.
 
+BLOCK DIAGRAM:
+
+1. Get Sensor Readings
+2. Check if dangerous levels have been reached and perform appropriate actions (e.g. beep a buzzer)
+3. Report readings back to base station along with safety status of the workers
+4. Repeat
+
+
+IMPORTANT:
+
+Emergency button should probably be implemented as an interrupt (if possible) to
+immediately interrupt any time-consuming running tasks.
+
 */
+
 
 
 
