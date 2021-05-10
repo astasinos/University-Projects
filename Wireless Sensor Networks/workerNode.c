2f7db78d42d3aa75e2bad39c7667403adbe5e2d4
astasinos <56511877+astasinos@users.noreply.github.com>
2021-05-10 13:18:16 +0300

    Update workerNode.c

--- Wireless Sensor Networks/workerNode.c
+++ Wireless Sensor Networks/workerNode.c
@@ -1,33 +1,35 @@
 /*
 
-Code that runs on the sensor nodes located on the helmets of the workers.
+   Code that runs on the sensor nodes located on the helmets of the workers.
 
-BLOCK DIAGRAM:
+   BLOCK DIAGRAM:
 
-1. Get Sensor Readings
-2. Check if dangerous levels have been reached and perform appropriate actions (e.g. beep a buzzer)
-3. Report readings back to base station along with safety status of the workers
-4. Repeat
+   1. Get Sensor Readings
+   2. Check if dangerous levels have been reached and perform appropriate actions (e.g. beep a buzzer)
+   3. Report readings back to base station along with safety status of the workers
+   4. Repeat
 
 
-IMPORTANT:
+   IMPORTANT:
 
-Emergency button should probably be implemented as an interrupt (if possible) to
-immediately interrupt any time-consuming running tasks.
+   Emergency button should probably be implemented as an interrupt (if possible) to
+   immediately interrupt any time-consuming running tasks.
 
-*/
+ */
 
-void getTemperature(){;}
+void getTemperature(){
+        ;
+}
 
 
 void setup(){
 
-;
+        ;
 
 }
 
 
 void loop(){
 
-  ;
+        ;
 }
